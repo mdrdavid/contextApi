79a4c29c4a9ec7857b54c4eadbe282fc74c97426
david <davidmatovu88@gmail.com>
2022-12-14 17:18:59 +0300

    add state using useState hook

--- context-app/src/components/Songlist.js
+++ context-app/src/components/Songlist.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import NewSongForm from "./NewSongForm";
 
 function SongList() {
-  return (
-    <div>
-      <ul>
-        <li> Song one</li>
-        <li> Song two</li>
-        <li> Song three</li>
-        <li> Song four</li>
-        <li> Song five</li>
-      </ul>
-     </div>
-  )
+  // using a useState hook
+	const [songs, setSongs] = useState([
+		{ title: "tuyimbe", id: 1 },
+		{ title: "we sing", id: 2 },
+		{ title: "raise me", id: 3 },
+		{ title: "come here", id: 4 },
+	]);
+  const addSong =(title)=>{
+    // setSongs([...songs, {title:"helo news", id: uuidv4()}])
+    setSongs([...songs, {title, id: uuidv4()}])
+  }
+	return (
+		<div>
+			<ul className="list-none">
+				{songs.map((song) => {
+					return <li key={song.id}>{song.title}</li>;
+				})}
+			</ul>
+      <button onClick={addSong}>Add Song</button>
+			<NewSongForm addSong={addSong}/>
+		</div>
+	);
 }
 
-export default SongList
+export default SongList;
